24db0671da2ce746ea19a14af6f77e82cbd47b16
Petrov Andrey <4ndrew1337@mail.ru>
2025-08-01 17:47:02 +0300

    add docker compose + makefile

--- internal/controller/http/v1/router.go
+++ internal/controller/http/v1/router.go
@@ -16,19 +16,20 @@ func NewSessionRoutes(apiV1Group fiber.Router, s usecase.Sessions, l logger.Inte
 	sessionGroup := apiV1Group.Group("/session")
 	userGroup := apiV1Group.Group("/user")
 
-	protectedSession := sessionGroup.Use(middleware.Auth(s))
-	protectedUser := userGroup.Use(middleware.Auth(s))
-
 	{ // /session
 		// open
 		sessionGroup.Post("/token", r.create)
 		sessionGroup.Post("/refresh", r.refresh)
 		// safe
+		protectedSession := sessionGroup.Use(middleware.Auth(s))
+
 		protectedSession.Post("/logout", r.logout)
 	}
 
 	{ // /user
 		// safe
+		protectedUser := userGroup.Use(middleware.Auth(s))
+
 		protectedUser.Get("/me", r.me)
 	}
 }
